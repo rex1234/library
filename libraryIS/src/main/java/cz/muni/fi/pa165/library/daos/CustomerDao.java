02bd4ea754502b1f06bb39be8fd4bb4cdc1cd8a6
vitmica <vit.mica@gmail.com>
2013-10-11 16:42:22 +0200

    Update CustomerDao.java

--- libraryIS/src/main/java/cz/muni/fi/pa165/library/daos/CustomerDao.java
+++ libraryIS/src/main/java/cz/muni/fi/pa165/library/daos/CustomerDao.java
@@ -1,16 +1,45 @@
 package cz.muni.fi.pa165.library.daos;
 
+/**
+ *
+ * @author vit.mica
+ */
+ 
 import cz.muni.fi.pa165.library.entities.Customer;
 import java.util.List;
 
-/**
- *
- * @author Mjartan
- */
 public interface CustomerDao {
-    void createCustomer(Customer book);
+    
+    /** 
+        * Inserts Customer into DB
+        * @param Customer to be stored
+    */
+    void createCustomer(Customer customer);
+    
+    /** 
+        * Returns all customers from DB
+        * @return  List of all Customers found in DB
+    */
     List<Customer> findAllCustomers();
-    Customer findBookById(Long id);
+    
+    
+    /** 
+        * Finds Customer with given ID
+        * @param long ID of customer to be found
+        * @return Customer with given ID or NULL if search was unsuccesful
+    */
+    Customer findCustomerById(Long id);
+    
+    /** 
+        * Deletes Customer in DB
+        * @param Customer to be deleted
+    */
     void deleteCustomer(Customer customer);
-    void updateCustomer(Customer customer);    
+    
+    /** 
+        * updates Customer in DB
+        * @param Customer to be updated
+    */
+    void updateCustomer(Customer customer);   
 }
+
