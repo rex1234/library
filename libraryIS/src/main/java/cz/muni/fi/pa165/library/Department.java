75cb02e516e3060ec7ac9948297bcb7a83180bbe
bzuco <bzuco.komar@gmail.com>
2013-10-11 23:58:40 +0200

    Create Department.java

---
+++ libraryIS/src/main/java/cz/muni/fi/pa165/library/Department.java
@@ -0,0 +1,10 @@
+package cz.muni.fi.pa165.library;
+
+/**
+ *
+ * @author MiškoHu
+ */
+public enum Department {
+    CHILDREN, FICTION, POETRY, HISTORY, RELIGION, MAGAZINES,
+    TRAVEL, SPORT, ECONOMY, DICTONARIES, SCIENCE, COOKING
+}
