7c64a560c259a339bacbd8e15746b0fd79716ac8
bzuco <bzuco.komar@gmail.com>
2013-10-11 23:53:35 +0200

    Update Impression.java

--- libraryIS/src/main/java/cz/muni/fi/pa165/library/entities/Impression.java
+++ libraryIS/src/main/java/cz/muni/fi/pa165/library/entities/Impression.java
@@ -1,9 +1,115 @@
 package cz.muni.fi.pa165.library.entities;
 
+import cz.muni.fi.libraryis.Department;
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import org.joda.time.LocalDate;
+
 /**
  *
- * @author 
+ * @author MiškoHu
  */
-public class Impression {
+@Entity
+public class Impression implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id; 
+    
+    @Column(nullable = false)
+    private String isbn;
+    
+    @Column(nullable = false)
+    private String name;    
+    
+    @Column(nullable = false)
+    private String author;    
+    
+    private LocalDate relaseDate;    
+    
+    private Department department;   
+    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public LocalDate getRelaseDate() {
+        return relaseDate;
+    }
+
+    public void setRelaseDate(LocalDate relaseDate) {
+        this.relaseDate = relaseDate;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 29 * hash + (this.id != null ? this.id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Impression other = (Impression) obj;
+        if (this.id != other.id && (this.id == null || !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }   
+
+    @Override
+    public String toString() {
+        return "Impression{" + "id=" + id + ", isbn=" + isbn + ", name=" + name + ", author=" + author + '}';
+    }
+    
     
 }
