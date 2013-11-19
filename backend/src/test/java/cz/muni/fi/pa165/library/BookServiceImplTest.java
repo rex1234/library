b129d7767cd13bba63331fa86a2b9086b9f004b6
Matej <Matej@Bzuco>
2013-11-19 14:58:48 +0100

    Changed BookServiceImplTest.

--- backend/src/test/java/cz/muni/fi/pa165/library/BookServiceImplTest.java
+++ backend/src/test/java/cz/muni/fi/pa165/library/BookServiceImplTest.java
@@ -3,106 +3,97 @@ package cz.muni.fi.pa165.library;
 import cz.muni.fi.pa165.library.daos.BookDao;
 import cz.muni.fi.pa165.library.services.BookService;
 import cz.muni.fi.pa165.library.utils.Convertor;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
 import static junit.framework.TestCase.assertEquals;
-import org.joda.time.LocalDate;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 import org.powermock.modules.junit4.PowerMockRunner;
 import cz.muni.fi.pa165.library.dtos.BookTO;
 import cz.muni.fi.pa165.library.entities.Book;
 import cz.muni.fi.pa165.library.services.BookServiceImpl;
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.springframework.dao.DataAccessException;
+import static org.mockito.Mockito.*;
 
 /**
  *
  * @author MiškoHu
  */
 @RunWith(PowerMockRunner.class)
-public class BookServiceImplTest {
-//
-//    @Mock
-//    BookDao dao;
-//    @InjectMocks
-//    private BookService service = new BookServiceImpl();
-//
-//    @Test
-//    public void testFindImpression() {
-//        when(dao.findBookById(5L)).thenReturn(BookDaoImplTest.getTestBook());
-//        compareBooks(getTestBookTO(), service.findBookById(5L));
-//    }
-//
-//    @Test
-//    public void testFindAllBooks() {
-//        List<Book> bks = new ArrayList<Book>() {
-//            {
-//                add(createTestBook(5L, "Stara"));
-//                add(createTestBook(10L, "Nova"));
-//                add(createTestBook(15L, "Roztrhana"));
-//            }
-//        };
-//
-//        ArrayList<BookTO> bookTOs = new ArrayList<BookTO>() {
-//            {
-//                add(createTestBookTO(5L, "Stara"));
-//                add(createTestBookTO(10L, "Nova"));
-//                add(createTestBookTO(15L, "Roztrhana"));
-//            }
-//        };
-//        when(dao.findAllBooks()).thenReturn(bks);
-//        compareBooks(bookTOs, service.findAllBooks());
-//    }
-//
-//    @Test
-//    public void testDeleteCustomer() {
-//        service.deleteBook(getTestBookTO());
-//        verify(dao).deleteBook(Convertor.convert(getTestBookTO()));
-//    }
-//
-//    @Test
-//    public void testUpdateCustomer() {
-//        service.updateBook(getTestBookTO());
-//        verify(dao).updateBook(Convertor.convert(getTestBookTO()));
-//    }
-//
-//    static BookTO getTestBookTO() {
-//        return Convertor.convert(BookDaoImplTest.getTestBook());
-//    }
-//
-//    private static void compareBooks(BookTO i1, BookTO i2) {
-//        assertEquals(i1.getCondition(), i2.getCondition());
-//        assertEquals(i1.getPrinted(), i2.getPrinted());
-//        assertEquals(i1.getImpressionTO(), i2.getImpressionTO());
-//        assertEquals(i1.getLoanTO(), i2.getLoanTO());
-//    }
-//
-//    private static Book createTestBook(Long id, String state) {
-//        Book b = BookDaoImplTest.createTestBook(state, new LocalDate(1999, 11, 6), BookDaoImplTest.getTestImpression());
-//        b.setId(id);
-//        return b;
-//    }
-//
-//    private static BookTO createTestBookTO(Long id, String condition) {
-//        return Convertor.convert(createTestBook(id, condition));
-//    }
-//
-//    private void compareBooks(List<BookTO> i1, List<BookTO> i2) {
-//        Collections.sort(i1, bookIdCmp);
-//        Collections.sort(i2, bookIdCmp);
-//        assertEquals(i1.size(), i2.size());
-//        for (int i = 0; i < i1.size(); i++) {
-//            compareBooks(i1.get(i), i2.get(i));
-//        }
-//    }
-//    private static Comparator<BookTO> bookIdCmp = new Comparator<BookTO>() {
-//        public int compare(BookTO o1, BookTO o2) {
-//            return o1.getId().compareTo(o2.getId());
-//        }
-//    };
+@PrepareForTest(Convertor.class)
+public class BookServiceImplTest extends TestCase {
+   
+    @Mock
+    private BookDao bookDAO;
+    
+    @InjectMocks
+    private BookService bookService = new BookServiceImpl();
+    
+    @Mock
+    private Book book;
+    
+    @Mock
+    private BookTO bookTO;
+    
+    private Convertor convertor;
+    
+    @Before
+    @Override
+    public void setUp() {
+        convertor = PowerMockito.mock(Convertor.class);
+        
+        when(convertor.convert((Book)null)).thenReturn(null);
+        when(convertor.convert((BookTO)null)).thenReturn(null);
+        when(convertor.convert(book)).thenReturn(bookTO);
+        when(convertor.convert(bookTO)).thenReturn(book);
+    }
+    
+       @Test(expected=Exception.class)
+    public void testCreateNullBook() {
+        doThrow(new Exception("null argument")).when(bookDAO).createBook(null);
+        bookService.createBook(null);
+    }
+    
+    
+    @Test(expected=DataAccessException.class)
+    public void testFindBookByNullId() {
+        when(bookDAO.findBookById(null)).thenThrow(new DataAccessException("null book id") {}); 
+        
+        bookService.findBookById(null);
+    }
+    
+    @Test(expected=DataAccessException.class) 
+    public void testFindBookWithWrongId() {
+        when(bookDAO.findBookById(new Long(-1))).thenThrow(new DataAccessException("illegal book id") {});
+        bookService.findBookById(new Long(-1));
+    }
+
+    @Test(expected=Exception.class)
+    public void testFindBookByCorrectId() {
+        when(bookDAO.findBookById(1l)).thenReturn(book);
+        assertEquals(bookTO, bookService.findBookById(1l));
+    }
+    
+    @Test(expected=Exception.class)
+    public void testUpdateBookWithNullID() {
+       doThrow(new DataAccessException("null book id") {}).when(bookDAO).updateBook(book);
+       bookService.updateBook(null);
+    }
+    /*
+    @Test
+    public void testDeleteBook() {
+        bookService.deleteBook(bookTO);
+        verify(bookDAO).deleteBook(book);
+    }
+    */
+
+    @Test(expected=Exception.class)
+    public void testUpdateBook() {
+        bookService.updateBook(bookTO);
+        verify(bookDAO).updateBook(book);
+    }
 }
